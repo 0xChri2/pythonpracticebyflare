709f2fef6097c405add65dc526cd49a2715849b1
ImChri2 <chris@chris-industries.de>
2022-06-09 15:32:36 +0200

    gameboard added tiktacktoe.py

--- tictaktoe/ticktacktoe.py
+++ tictaktoe/ticktacktoe.py
@@ -8,7 +8,10 @@ layoutlogin =[[sg.Text('Tic Tac Toe')],
         [sg.Button('Play')]]
 
 layoutgame = layout =[[sg.Text('Tic Tac Toe')],
-            [sg.Text('Player 1:', size=(10, 1)),sg.Text(key='-OUT-', size=(10, 1)),sg.Text('Player 2:', size=(10, 1)),sg.Text(key='-OUT2-', size=(10, 1))]]
+            [sg.Text('Player 1:', size=(10, 1)),sg.Text(key='-OUT-', size=(10, 1)),sg.Text('Player 2:', size=(10, 1)),sg.Text(key='-OUT2-', size=(10, 1))],
+            [sg.Button('1', size=(10,5)), sg.Button('2', size=(10, 5)), sg.Button('3', size=(10, 5))], 
+            [sg.Button('4', size=(10,5)), sg.Button('5', size=(10,5)), sg.Button('6', size=(10,5))],
+            [sg.Button('7', size=(10,5)), sg.Button('8', size=(10,5)), sg.Button('9', size=(10,5))]]
 
 layout = [[sg.Column(layoutlogin, key='-COL1-', visible=True), sg.Column(layoutgame, visible=False, key='-COL2-')]]
 
@@ -24,6 +27,7 @@ while True:
         window['-COL2-'].update(visible=True)
         window['-OUT-'].update(values['-IN-'])
         window['-OUT2-'].update(values['-IN2-'])
+
     else: 
         break
         window.close()
