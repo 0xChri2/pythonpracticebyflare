b612e7c4fc9435f1d6d4319b5e163328b0f4e8c6
ImChri2 <chris@chris-industries.de>
2022-10-23 21:43:53 +0200

    Added datatypetest.py

---
+++ studies/Testing/datatyptest.py
@@ -0,0 +1,16 @@
+anfang_des_alphabets: float = "abc" 
+wie_viel_ist_vom_kuchen_uebrig: bool = 0.5
+wahr: int = True
+fuenfzig: str = 50
+
+
+anfang_des_alphabets: str = "abc"
+wie_viel_ist_vom_kuchen_uebrig: float = 0.5
+wahr: bool = True
+fuenfzig: int = 50
+
+
+zahlen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for zahl in zahlen:
+    print(zahl)
