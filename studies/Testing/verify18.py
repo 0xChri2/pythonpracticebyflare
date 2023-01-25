a1adaca67750a64af548baf76de99ef2c6300736
ImChri2 <chris@chris-industries.de>
2023-01-25 21:41:31 +0100

    added verify18.py

---
+++ studies/Testing/verify18.py
@@ -0,0 +1,13 @@
+peranz = input("Wie viele Personen nehmen an der Berfragung Teil?")
+count  = 0
+persu18 = {}
+while(count < int(peranz)):
+    alter = input("Wie alt bist du?")
+    name = input("Wie ist dein Name?")
+    if(int(alter) < 18):
+        count = count + 1
+        persu18.update({name: int(alter)})
+    else:
+        count = count + 1
+print(persu18)
+        
